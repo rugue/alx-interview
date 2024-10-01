5c427fab9d1051ddcc5d0588d3dfa62e02efbb40
Osarugue Enehizena <nehirugue@gmail.com>
2024-10-01 07:34:29 +0100

    Refactor pascal_triangle function to generate Pascal's Triangle up to n rows

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,16 +1,32 @@
+#!/usr/bin/python3
+"""Pascal's Triangle"""
+
+
 def pascal_triangle(n):
+    """
+    a function that returns a list
+    of integers representing the
+    pascal triangle of n:
+       . Returns an empty list if n <= 0
+       . assume n will be always an integer
+    """
+    pascal_tri = []
+
     if n <= 0:
         return []
 
-    triangle = [[1]]  # First row of Pascal's triangle
+    for i in range(n):
+        if (i == 0):
+            pascal_tri.append([1])
+        else:
+            cur_row = []
+            for j in range(i + 1):
+                if (j == 0 or j == i):
+                    cur_row.append(1)
+                else:
+                    cur_row.append(pascal_tri[i - 1][j - 1] +
+                                   pascal_tri[i - 1][j])
 
-    for i in range(1, n):
-        prev_row = triangle[-1]  # Get the last row
-        new_row = [1]  # Every row starts with 1
-        # Generate the middle values of the row
-        for j in range(1, i):
-            new_row.append(prev_row[j-1] + prev_row[j])
-        new_row.append(1)  # Every row ends with 1
-        triangle.append(new_row)
+            pascal_tri.append(cur_row)
 
-    return triangle
+    return (pascal_tri)
