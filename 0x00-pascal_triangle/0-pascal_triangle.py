9e58bccf28170d262e0b95d4f626afeb6c9ab6a0
Osarugue Enehizena <nehirugue@gmail.com>
2024-10-01 07:26:18 +0100

    Refactor pascal_triangle function to generate Pascal's Triangle up to n rows

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,48 +1,24 @@
 #!/usr/bin/python3
-"""Pascal's Triangle"""
+"""
+0-pascal_triangle
+"""
 
 
 def pascal_triangle(n):
     """
-    a function that returns a list
-    of integers representing the
-    pascal triangle of n:
-       . Returns an empty list if n <= 0
-       . assume n will be always an integer
+    Returns a list of integers
+    representing the Pascal Triangle of n
+    returns empty list if n <= 0
     """
-    pascal_tri = []
-
+    k = []
     if n <= 0:
-        return []
-
-    for i in range(n):
-        if (i == 0):
-            pascal_tri.append([1])
-        else:
-            cur_row = []
-            for j in range(i + 1):
-                if (j == 0 or j == i):
-                    cur_row.append(1)
-                else:
-                    cur_row.append(pascal_tri[i - 1][j - 1] +
-                                   pascal_tri[i - 1][j])
-
-            pascal_tri.append(cur_row)
-
-    return (pascal_tri)def pascal_triangle(n):
-    if n <= 0:
-        return []
-
-    triangle = [[1]]  # Start with the first row
-
+        return k
+    k = [[1]]
     for i in range(1, n):
-        prev_row = triangle[-1]
-        new_row = [1]  # First element is always 1
-
-        for j in range(1, i):
-            new_row.append(prev_row[j-1] + prev_row[j])
-
-        new_row.append(1)  # Last element is always 1
-        triangle.append(new_row)
-
-    return triangle
+        temp = [1]
+        for j in range(len(k[i - 1]) - 1):
+            curr = k[i - 1]
+            temp.append(k[i - 1][j] + k[i - 1][j + 1])
+        temp.append(1)
+        k.append(temp)
+    return k
